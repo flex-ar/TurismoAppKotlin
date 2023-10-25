0deeab894851aaff51d7c319114d2404c92e2584
flex-ar <jmarnedo8@gmail.com>
2023-10-25 17:55:55 -0300

    add popup

---
+++ app/src/main/java/com/example/turismo/ui/utils/PopupManager.kt
@@ -0,0 +1,82 @@
+package com.example.turismo.ui.utils
+
+import android.app.AlertDialog
+import android.content.Context
+import android.media.MediaPlayer
+import android.os.Handler
+import android.os.Message
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import com.example.turismo.databinding.PopupLayoutBinding
+
+object PopupManager {
+  var isActive = false;
+
+  fun showPopup(context: Context, music: Int) {
+    isActive = true
+    val dialogBuilder = AlertDialog.Builder(context)
+    val binding = PopupLayoutBinding.inflate(LayoutInflater.from(context))
+    dialogBuilder.setView(binding.root)
+    val alertDialog = dialogBuilder.create()
+    alertDialog.setCanceledOnTouchOutside(false)
+    val mediaPlayer = MediaPlayer.create(context, music)
+    var isThreadRunning = true
+
+    binding.toggleButton.addOnButtonCheckedListener { _, checkedId, isChecked ->
+      if (isChecked) {
+        when (checkedId) {
+          binding.playButton.id -> {
+            mediaPlayer.start()
+            binding.playButton.visibility = View.GONE
+            binding.pauseButton.visibility = View.VISIBLE
+          }
+
+          binding.pauseButton.id -> {
+            mediaPlayer.pause()
+            binding.pauseButton.visibility = View.GONE
+            binding.playButton.visibility = View.VISIBLE
+          }
+        }
+      }
+    }
+
+    binding.slider.valueFrom = 0f
+    binding.slider.valueTo = mediaPlayer.duration.toFloat()
+
+    binding.slider.addOnChangeListener { _, value, fromUser ->
+      if (fromUser) mediaPlayer.seekTo(value.toInt())
+    }
+
+    val handler = object : Handler() {
+      override fun handleMessage(msg: Message) {
+        val currentPosition = msg.what
+        binding.slider.value = currentPosition.toFloat()
+      }
+    }
+
+    Thread(Runnable {
+      while (isThreadRunning) {
+        try {
+          if (!isThreadRunning) break
+          val msg = Message()
+          if (mediaPlayer.currentPosition >= 0 && mediaPlayer.currentPosition <= mediaPlayer.duration) {
+            msg.what = mediaPlayer.currentPosition
+            handler.sendMessage(msg)
+          }
+          Thread.sleep(1000)
+        } catch (e: InterruptedException) {
+          Log.d("Thread", e.message.toString())
+        }
+      }
+    }).start()
+
+    binding.buttonClose.setOnClickListener {
+      mediaPlayer.release()
+      isThreadRunning = false
+      isActive = false
+      alertDialog.dismiss()
+    }
+    alertDialog.show()
+  }
+}
